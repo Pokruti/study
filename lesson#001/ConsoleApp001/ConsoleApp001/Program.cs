9e60cbcab652b80f7e14add392a4b0f369d1dff7
Pokruti <firlam@yandex.ru>
2021-04-24 19:25:28 +0300

    added ConsoleApp

---
+++ lesson#001/ConsoleApp001/ConsoleApp001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp001
+{
+    class Program001
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What's your name?\n");
+            Console.Write("Name: ");
+            string nName = Console.ReadLine();
+            Console.WriteLine($"\nNice to meet you {nName}! Today's {DateTime.Now}.");
+            Console.ReadKey();
+        }
+    }
+}
