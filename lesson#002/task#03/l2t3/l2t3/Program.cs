a3f4a042855736410afbac5500645ae7fecd16ee
Pokruti <firlam@yandex.ru>
2021-05-01 07:41:57 +0300

    The solution for the task3 was added

---
+++ lesson#002/task#03/l2t3/l2t3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace l2t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enter:
+            Console.WriteLine("Enter the number please:");
+            double parNumber = Convert.ToDouble(Console.ReadLine());
+            if (parNumber % 2 == 0)
+            {
+                Console.WriteLine($"{parNumber} is even");
+                goto Enter;
+            }
+            else
+            {
+                Console.WriteLine($"{parNumber} is odd");
+                goto Enter;
+            }
+        }
+    }
+}
