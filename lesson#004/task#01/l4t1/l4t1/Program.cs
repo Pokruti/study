ecf71f1b9983e36f8357660a652069caad360cee
Pokruti <firlam@yandex.ru>
2021-05-10 22:30:20 +0300

    The task#01 was added

---
+++ lesson#004/task#01/l4t1/l4t1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace l4t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            static string GetFullName(string FirstName, string LastName, string patronymic)
+            {
+                string parSeparator = " "; //разделитель
+               
+                string strReturn = string.Join(parSeparator, FirstName,LastName,patronymic); //Объединение строк
+
+                return strReturn;
+            }
+
+            int i;
+            
+            string[,] arrMem = new string[3, 1]; //Объявляем массив для хранения ФИО
+            for (i = 0; i < 3; i++)              //По циклу вводим ФИО
+            {
+                Console.WriteLine("\nВведите фамилию:");
+                var FirstName = Console.ReadLine();
+                Console.WriteLine("Введите имя:");
+                var LastName = Console.ReadLine();
+                Console.WriteLine("Введите отчество:");
+                var patronymic = Console.ReadLine();
+                arrMem[i, 0] = GetFullName(FirstName, LastName, patronymic); //Записываем ФИО
+                Console.Clear();
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($"\n{arrMem[j, 0]}\n"); 
+                }
+            }
+        }
+    }
+}
