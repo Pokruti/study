8c026819cb62cd86a2356f7c750c293decdc5bba
Pokruti <firlam@yandex.ru>
2021-05-16 23:30:55 +0300

    Task#02 was added

---
+++ lesson#005/task#02/l5t2/l5t2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace l5t2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program writes the time in file.");
+            funcWriteTime();
+        }
+        static void funcWriteTime()
+        {
+            string strFileName = "startup.txt";                                  //Задаём путь к файлу
+            string strDate = Convert.ToString(DateTime.Now.ToString("HH:mm:ss"));//Форматируем время
+            File.AppendAllText(strFileName, strDate);                            //Добавляем строку в файл
+            File.AppendAllText(strFileName, Environment.NewLine);                //Делаем перенос строки в файле
+
+        }
+
+    }
+}
