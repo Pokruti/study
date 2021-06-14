83248aa168a2e3c43640c8481ca99bfb23e4e3d7
Pokruti <firlam@yandex.ru>
2021-06-14 18:27:46 +0300

    Lesson 9

---
+++ lesson#009/FileManager/FM/FM/Panelka.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileManager
+{
+    class FilePanel
+    {
+
+
+        public static int PANEL_HEIGHT = 20; //ограничение высоты панели
+        public static int PANEL_WIDTH = 70; //ограничение длины панели
+
+
+        //Настройка панелек
+
+        private int top;
+        public int Top
+        {
+            get
+            {
+                return this.top;
+            }
+            set
+            {                
+                this.top = 0;
+            }
+        }
+
+        private int left;
+        public int Left
+        {
+            get
+            {
+                return this.left;
+            }
+            set
+            {
+                this.left = value;
+            }
+        }
+
+        
+
+        private int height = FilePanel.PANEL_HEIGHT;     
+        private int width = FilePanel.PANEL_WIDTH;
+       
+
+
+        
+        private string path;
+        public string Path
+        {
+            get
+            {
+                return this.path;
+            }
+            set
+            {
+                DirectoryInfo di = new DirectoryInfo(value);
+                if (di.Exists)
+                {
+                    this.path = value;
+                }
+                else
+                {
+                    throw new Exception(String.Format("Путь {0} не существует", value));
+                }
+            }
+        }
+
+        private int activeObjectIndex = 0;
+        private int firstObjectIndex = 0;
+        private int displayedObjectsAmount = PANEL_HEIGHT - 2; //Длина постраничного списка вывода
+        private bool active;
+        public bool Active
+        {
+            get
+            {
+                return this.active;
+            }
+            set
+            {
+                this.active = value;
+            }
+        }
+        private bool discs;
+        public bool isDiscs
+        {
+            get
+            {
+                return this.discs;
+            }
+        }
+
+       
+        private List<FileSystemInfo> fsObjects = new List<FileSystemInfo>();
+
+        
+        public FilePanel()
+        {
+            this.SetDiscs();
+        }
+
+        public FilePanel(string path)
+        {
+            this.path = path;
+            this.SetLists();
+        }
+
+
+
+        public FileSystemInfo GetActiveObject()
+        {
+            if (this.fsObjects != null && this.fsObjects.Count != 0)
+            {
+                return this.fsObjects[this.activeObjectIndex];
+            }
+            throw new Exception("Список объектов панели пуст");
+        }
+
+
+
+        //Навигация
+
+        public void KeyboardProcessing(ConsoleKeyInfo key)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    this.ScrollUp();
+                    break;
+                case ConsoleKey.DownArrow:
+                    this.ScrollDown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ScrollDown() //Скроллирование
+        {
+            if (this.activeObjectIndex >= this.firstObjectIndex + this.displayedObjectsAmount - 1)
+            {
+                this.firstObjectIndex += 1;
+                if (this.firstObjectIndex + this.displayedObjectsAmount >= this.fsObjects.Count)
+                {
+                    this.firstObjectIndex = this.fsObjects.Count - this.displayedObjectsAmount;
+                }
+                this.activeObjectIndex = this.firstObjectIndex + this.displayedObjectsAmount - 1;
+                this.UpdateContent(false);
+
+            }
+
+            else
+            {
+                if (this.activeObjectIndex >= this.fsObjects.Count - 1)
+                {
+                    return;
+                }
+                this.DeactivateObject(this.activeObjectIndex);
+                this.activeObjectIndex++;
+                this.ActivateObject(this.activeObjectIndex);
+            }
+        }
+
+        private void ScrollUp() //Скроллирование
+        {
+            if (this.activeObjectIndex <= this.firstObjectIndex)
+            {
+                this.firstObjectIndex -= 1;
+                if (this.firstObjectIndex < 0)
+                {
+                    this.firstObjectIndex = 0;
+                }
+                this.activeObjectIndex = firstObjectIndex;
+                this.UpdateContent(false);
+            }
+            else
+            {
+                this.DeactivateObject(this.activeObjectIndex);
+                this.activeObjectIndex--;
+                this.ActivateObject(this.activeObjectIndex);
+            }
+        }
+
+
+
+
+
+
+
+        public void SetLists() //Получение данных в панели
+        {
+            if (this.fsObjects.Count != 0)
+            {
+                this.fsObjects.Clear();
+            }
+
+            this.discs = false; //Если не диски, то заполняем директориями и файлами
+
+            DirectoryInfo levelUpDirectory = null;
+            this.fsObjects.Add(levelUpDirectory);
+
+            //Директорий
+
+            string[] directories = Directory.GetDirectories(this.path);
+            foreach (string directory in directories)
+            {
+                DirectoryInfo di = new DirectoryInfo(directory);
+                this.fsObjects.Add(di);
+            }
+
+            //Файлов
+
+            string[] files = Directory.GetFiles(this.path);
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                this.fsObjects.Add(fi);
+            }
+        }
+
+        public void SetDiscs()
+        {
+            if (this.fsObjects.Count != 0)
+            {
+                this.fsObjects.Clear();
+            }
+
+            this.discs = true; //Если вышли в корень
+
+            DriveInfo[] discs = DriveInfo.GetDrives();
+            foreach (DriveInfo disc in discs)
+            {
+                if (disc.IsReady)
+                {
+                    DirectoryInfo di = new DirectoryInfo(disc.Name);
+                    this.fsObjects.Add(di);
+                }
+            }
+        }
+
+
+
+
+        //Рисовашки
+        public void Show()
+        {
+            this.Clear();
+
+            this.PrintContent();
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.height; i++)
+            {
+                string space = new String(' ', this.width);
+                Console.SetCursorPosition(this.left, this.top + i);
+                Console.Write(space);
+            }
+        }
+
+        private void PrintContent()
+        {
+            if (this.fsObjects.Count == 0)
+            {
+                return;
+            }
+            int count = 0;
+
+            int lastElement = this.firstObjectIndex + this.displayedObjectsAmount;
+            if (lastElement > this.fsObjects.Count)
+            {
+                lastElement = this.fsObjects.Count;
+            }
+
+
+            if (this.activeObjectIndex >= this.fsObjects.Count)
+            {
+                activeObjectIndex = 0;
+            }
+
+            for (int i = this.firstObjectIndex; i < lastElement; i++)
+            {
+                Console.SetCursorPosition(this.left + 1, this.top + count + 1);
+
+                if (i == this.activeObjectIndex && this.active == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                this.PrintObject(i);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                count++;
+            }
+        }
+
+        private void ClearContent()
+        {
+            for (int i = 1; i < this.height - 1; i++)
+            {
+                string space = new String(' ', this.width - 2);
+                Console.SetCursorPosition(this.left + 1, this.top + i);
+                Console.Write(space);
+            }
+        }
+
+        private void PrintObject(int index) // Рисуем в панелях
+        {
+            if (index < 0 || this.fsObjects.Count <= index)
+            {
+                throw new Exception(String.Format("Невозможно вывести объект c индексом {0}. Выход индекса за диапазон", index));
+            }
+
+            int currentCursorTopPosition = Console.CursorTop;
+            int currentCursorLeftPosition = Console.CursorLeft;
+
+            if (!this.discs && index == 0)
+            {
+                Console.Write("..");
+                return;
+            }
+
+            Console.Write("{0}", fsObjects[index].Name); //Названия
+            Console.SetCursorPosition(currentCursorLeftPosition + this.width / 2, currentCursorTopPosition);
+            if (fsObjects[index] is DirectoryInfo)
+            {
+                Console.Write("{0}", ((DirectoryInfo)fsObjects[index]).LastWriteTime);//Даты
+            }
+            else
+            {
+                Console.Write("{0}", ((FileInfo)fsObjects[index]).Length);//Размер
+            }
+        }
+
+        public void UpdatePanel()
+        {
+            this.firstObjectIndex = 0;
+            this.activeObjectIndex = 0;
+            this.Show();
+        }
+
+        public void UpdateContent(bool updateList)
+        {
+            if (updateList)
+            {
+                this.SetLists();
+            }
+            this.ClearContent();
+            this.PrintContent();
+        }
+
+        private void ActivateObject(int index) //При скроллировании
+        {
+            int offsetY = this.activeObjectIndex - this.firstObjectIndex;
+            Console.SetCursorPosition(this.left + 1, this.top + offsetY + 1);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            this.PrintObject(index);
+                        
+        }
+
+        private void DeactivateObject(int index)
+        {
+            int offsetY = this.activeObjectIndex - this.firstObjectIndex;
+            Console.SetCursorPosition(this.left + 1, this.top + offsetY + 1);
+
+            this.PrintObject(index);
+        }
+
+
+    }
+}
---
+++ lesson#009/FileManager/FM/FM/Program.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileManager
+{
+    public delegate void OnKey(ConsoleKeyInfo key);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager manager = new FileManager();
+            manager.Start();
+        }
+    }
+
+
+    
+
+
+    
+        class FileManager
+        {
+
+            
+            public event OnKey KeyPress;
+            List<FilePanel> panels = new List<FilePanel>();
+            private int activePanelIndex;
+
+            static FileManager()
+            {
+                Console.CursorVisible = false;  //Настройка окна консоли
+                Console.SetWindowSize(150, 35);
+                Console.SetBufferSize(150, 35);
+            }
+
+            public FileManager()
+            {
+                FilePanel filePanel = new FilePanel(); //расположение первой панелей
+                filePanel.Top = 0;
+                filePanel.Left = 0;
+                this.panels.Add(filePanel);
+
+                filePanel = new FilePanel(); // расположение второй панели
+                filePanel.Top = 0;
+                filePanel.Left = 80;
+                this.panels.Add(filePanel);
+
+                activePanelIndex = 0; //выбор активного окна
+
+                this.panels[this.activePanelIndex].Active = true;
+                KeyPress += this.panels[this.activePanelIndex].KeyboardProcessing;
+
+                foreach (FilePanel fp in panels)
+                {
+                    fp.Show();
+                }
+
+                this.ShowKeys();
+            }
+
+
+
+            public void Start()
+            {
+                bool exit = false;
+                while (!exit)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        this.ClearMessage();
+
+                        ConsoleKeyInfo userKey = Console.ReadKey(true);
+                        switch (userKey.Key)
+                        {
+                            case ConsoleKey.Tab:
+                                this.ChangeActivePanel();
+                                break;
+                            case ConsoleKey.Enter:
+                                this.ChangeDirectory();
+                                break;
+                            case ConsoleKey.F1:
+                                this.Copy();
+                                break;
+                            case ConsoleKey.F2:
+                                this.CreateDirectory();
+                                break;
+                            case ConsoleKey.F3:
+                                this.Delete();
+                                break;
+                            case ConsoleKey.F10:
+                                exit = true;
+                                Console.ResetColor();
+                                Console.Clear();
+                                break;
+                            case ConsoleKey.DownArrow:
+                                goto case ConsoleKey.PageUp;
+                            case ConsoleKey.UpArrow:
+                                goto case ConsoleKey.PageUp;
+                            case ConsoleKey.PageUp:
+                                this.KeyPress(userKey);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+
+            private string AksName(string message) //Запрос названия
+            {
+                string name;
+                Console.CursorVisible = true;
+                do
+                {
+                    this.ClearMessage();
+                    this.ShowMessage(message);
+                    name = Console.ReadLine();
+                } while (name.Length == 0);
+                Console.CursorVisible = false;
+                this.ClearMessage();
+                return name;
+            }
+
+
+            //Операции
+            private void Copy() //Копирование
+            {
+                foreach (FilePanel panel in panels)
+                {
+                    if (panel.isDiscs)
+                    {
+                        return;
+                    }
+                }
+
+                if (this.panels[0].Path == this.panels[1].Path)
+                {
+                    return;
+                }
+
+                try
+                {
+                    string destPath = this.activePanelIndex == 0 ? this.panels[1].Path : this.panels[0].Path;
+
+                    FileSystemInfo fileObject = this.panels[this.activePanelIndex].GetActiveObject();
+                    FileInfo currentFile = fileObject as FileInfo;
+
+                    if (currentFile != null) //Копируем файл
+                    {
+                        string fileName = currentFile.Name;
+                        string destName = Path.Combine(destPath, fileName);
+                        File.Copy(currentFile.FullName, destName, true);
+                    }
+
+                    else
+                    {
+                        string currentDir = ((DirectoryInfo)fileObject).FullName;
+                        string destDir = Path.Combine(destPath, ((DirectoryInfo)fileObject).Name);
+                        CopyDirectory(currentDir, destDir);//Если не файл, тогда директорию
+                    }
+
+                    this.RefreshPannels();
+                }
+                catch (Exception e)
+                {
+                    this.ShowMessage(e.Message);
+                    return;
+                }
+            }
+
+            private void CopyDirectory(string sourceDirName, string destDirName) //Функция копирования каталога
+            {
+                DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
+
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, true);
+                }
+
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    CopyDirectory(subdir.FullName, temppath);
+                }
+            }
+
+            private void Delete() //Удаление
+            {
+                if (this.panels[this.activePanelIndex].isDiscs)
+                {
+                    return;
+                }
+
+                FileSystemInfo fileObject = this.panels[this.activePanelIndex].GetActiveObject();
+                try
+                {
+                    if (fileObject is DirectoryInfo)
+                    {
+                        ((DirectoryInfo)fileObject).Delete(true);
+                    }
+                    else
+                    {
+                        ((FileInfo)fileObject).Delete();
+                    }
+                    this.RefreshPannels();
+                }
+                catch (Exception e)
+                {
+                    this.ShowMessage(e.Message);
+                    return;
+                }
+            }
+
+            private void CreateDirectory() //Создание каталога
+            {
+                if (this.panels[this.activePanelIndex].isDiscs)
+                {
+                    return;
+                }
+
+                string destPath = this.panels[this.activePanelIndex].Path;
+                string dirName = this.AksName("Введите имя каталога: ");
+
+                try
+                {
+                    string dirFullName = Path.Combine(destPath, dirName);
+                    DirectoryInfo dir = new DirectoryInfo(dirFullName);
+                    if (!dir.Exists)
+                    {
+                        dir.Create();
+                    }
+                    else
+                    {
+                        this.ShowMessage("Каталог с таким именем уже существует");
+                    }
+                    this.RefreshPannels();
+                }
+                catch (Exception e)
+                {
+                    this.ShowMessage(e.Message);
+                }
+            }
+
+
+
+
+
+            private void RefreshPannels() //Обновление панелей
+            {
+                if (this.panels == null || this.panels.Count == 0)
+                {
+                    return;
+                }
+
+                foreach (FilePanel panel in panels)
+                {
+                    if (!panel.isDiscs)
+                    {
+                        panel.UpdateContent(true);
+                    }
+                }
+            }
+
+            private void ChangeActivePanel() //Смена активной панели
+            {
+                this.panels[this.activePanelIndex].Active = false;
+                KeyPress -= this.panels[this.activePanelIndex].KeyboardProcessing;
+                this.panels[this.activePanelIndex].UpdateContent(false);
+
+                this.activePanelIndex++;
+                if (this.activePanelIndex >= this.panels.Count)
+                {
+                    this.activePanelIndex = 0;
+                }
+
+                this.panels[this.activePanelIndex].Active = true;
+                KeyPress += this.panels[this.activePanelIndex].KeyboardProcessing;
+                this.panels[this.activePanelIndex].UpdateContent(false);
+            }
+
+            private void ChangeDirectory() //Смена директории
+            {
+                FileSystemInfo fsInfo = this.panels[this.activePanelIndex].GetActiveObject();
+                if (fsInfo != null)
+                {
+                    if (fsInfo is DirectoryInfo)
+                    {
+                        try
+                        {
+                            Directory.GetDirectories(fsInfo.FullName);
+                        }
+                        catch
+                        {
+                            return;
+                        }
+
+                        this.panels[this.activePanelIndex].Path = fsInfo.FullName;
+                        this.panels[this.activePanelIndex].SetLists();
+                        this.panels[this.activePanelIndex].UpdatePanel();
+                    }
+
+                }
+                else
+                {
+                    string currentPath = this.panels[this.activePanelIndex].Path;
+                    DirectoryInfo currentDirectory = new DirectoryInfo(currentPath);
+                    DirectoryInfo upLevelDirectory = currentDirectory.Parent;
+
+                    if (upLevelDirectory != null)
+                    {
+                        this.panels[this.activePanelIndex].Path = upLevelDirectory.FullName;
+                        this.panels[this.activePanelIndex].SetLists();
+                        this.panels[this.activePanelIndex].UpdatePanel();
+                    }
+
+                    else
+                    {
+                        this.panels[this.activePanelIndex].SetDiscs();
+                        this.panels[this.activePanelIndex].UpdatePanel();
+                    }
+                }
+            }
+
+
+
+            public static void PrintString(string str, int X, int Y, ConsoleColor text, ConsoleColor background) //Функция рисования для кнопок и сообщения
+            {
+                Console.ForegroundColor = text;
+                Console.BackgroundColor = background;
+
+                Console.SetCursorPosition(X, Y);
+                Console.Write(str);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+
+            private void ShowKeys() //Полоска с действиями
+            {
+
+                string[] menu = { "F1 Копия", "F2 Создать каталог", "F3 Удаление", "F10 Выход" };
+
+
+                int cellTop = 20;
+                int cellWidth = 20;
+
+
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    PrintString(menu[i], i * cellWidth, cellTop, ConsoleColor.Green, ConsoleColor.Black); //Вывод подсказок о кнопках управления
+                }
+            }
+
+            private void ShowMessage(string message) //Сообщение при создании каталога
+            {
+                PrintString(message, 0, Console.WindowHeight - 10, ConsoleColor.Green, ConsoleColor.Black);
+            }
+
+            private void ClearMessage() //Его очистка
+            {
+                PrintString(new String(' ', Console.WindowWidth), 0, Console.WindowHeight - 10, ConsoleColor.Green, ConsoleColor.Black);
+            }
+
+        
+
+    }
+
+}
+
